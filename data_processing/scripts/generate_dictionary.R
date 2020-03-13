15e52e92411fdb5c226be612058f1a4660407781
Ricardo Ramírez <rramirez@lcg.unam.mx>
2020-03-13 14:15:55 +0100

    updates general ids

---
+++ data_processing/scripts/generate_dictionary.R
@@ -0,0 +1,20 @@
+# Copyright (c) [2019] [Ricardo O. Ramirez Flores]
+# roramirezf@uni-heidelberg.de
+
+#' Description: Generates dictionary of studies/IDs
+#' 
+#' 
+
+GEO_ID = c("GSE57345","GSE5406","Translatome","PRJNA477855",
+           "PRJNA198165","PRJNA522417","GSE1869","GSE55296",
+           "GSE42955","GSE26887","GSE3585","PRJNA291619",
+           "PRJNA246308","GSE76701","GSE16499")
+
+newID = c("Liu15_M","Hannenhalli06","vanHeesch19","Sweet18","Yang14",
+          "Spurrell19","Kittleson05","Tarazon14","Molina-Navarro13","Greco12",     
+          "Barth06","Schiano17","Liu15_R","Kim16","Kong10")
+
+dictionary = data.frame(GEO_ID, newID,
+                        stringsAsFactors = F)
+
+save(dictionary, file = "data/dictionaryIDs.ro")
