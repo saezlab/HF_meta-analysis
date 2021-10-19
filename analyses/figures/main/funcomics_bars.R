fc6a8c7eef411aa5ef8ab7cb9123b47dfa662e76
Ricardo Ramírez <roramirezf@uni-heidelberg.de>
2021-10-19 13:47:56 +0200

    fixed p-val correction to paper version

--- analyses/figures/main/funcomics_bars.R
+++ analyses/figures/main/funcomics_bars.R
@@ -1,4 +1,4 @@
-# Copyright (c) [2019] [Ricardo O. Ramirez Flores]
+# Copyright (c) [2020] [Ricardo O. Ramirez Flores]
 # roramirezf@uni-heidelberg.de
 
 #' In this script we plot the results of funcomics 
@@ -18,24 +18,21 @@ miRNA = readRDS(file = "data/figure_objects/GSEA_mir_results.rds")
 # GSEA filtering
 
 GSEA_filtering = GSEA %>% 
-  arrange(desc(abs(NES))) %>%
+  arrange(padj_global) %>%
   slice(1:50) %>%
-  arrange(-log10(pval) * sign(NES)) %>%
+  arrange(-log10(padj_global) * sign(NES)) %>%
   mutate(pathway = strtrim(pathway,40)) %>%
   mutate(dir_reg = sign(ES),
-         log_pval = -log10(pval),
-         gset = pathway) %>%
-  mutate(gset = ifelse(padj<=0.15,
-                       paste("*",gset,sep=""),
-                       gset))
+         log_pval = -log10(padj_global),
+         gset = pathway)
 
 GSEA_filtering$gset = factor(GSEA_filtering$gset,
                              levels = GSEA_filtering$gset)
 
-g1 = ggplot(GSEA_filtering, aes(x=gset, y=-log10(pval),
-                                 fill = factor(dir_reg))) + 
+g1 = ggplot(GSEA_filtering, aes(x=gset, y=log_pval,
+                                fill = factor(dir_reg))) + 
   geom_bar(stat = "identity") + theme_minimal() + 
-  geom_hline(yintercept = -log10(0.05),linetype="dotted" ) +
+  geom_hline(yintercept = -log10(0.25),linetype="dotted" ) +
   coord_flip() +
   theme(axis.ticks.y = element_blank(),
         axis.title.y = element_blank(),
@@ -44,30 +41,27 @@ g1 = ggplot(GSEA_filtering, aes(x=gset, y=-log10(pval),
         legend.position = "none",
         axis.title.x = element_text(size = 9),
         #plot.margin = unit(c(0, 0, 0, 0), 'cm')
-        ) + ylab("-log10(p-value)") +
+  ) + ylab("-log10(BH p-value)") +
   scale_fill_manual(values = c("steelblue","indianred"))
-  
+
 
 # Dorothea_filtering
 
 dorothea_filtering = dorothea %>% dplyr::slice(1:50) %>%
   #dplyr::filter(pvalue < 0.05) %>%
-  arrange(-log10(pvalue) * sign(NES)) %>%
+  arrange(-log10(adj_pvalue) * sign(NES)) %>%
   mutate(dir_reg = sign(NES),
-         log_pval = -log10(pvalue),
+         log_pval = -log10(adj_pvalue),
          dir_col = dir_reg * log_pval,
-         gset = RegulonName) %>%
-    mutate(gset = ifelse(adj_pvalue<=0.15,
-                         paste("*",gset,sep=""),
-                         gset))
+         gset = RegulonName)
 
 dorothea_filtering$gset = factor(dorothea_filtering$gset,
                                  levels = dorothea_filtering$gset)
 
-g2 <- ggplot(dorothea_filtering, aes(x=gset, y=-log10(pvalue),
-                                 fill = factor(sign(NES)))) + 
+g2 <- ggplot(dorothea_filtering, aes(x=gset, y=log_pval,
+                                     fill = factor(sign(NES)))) + 
   geom_bar(stat = "identity") + theme_minimal() +
-  geom_hline(yintercept = -log10(0.05),linetype="dotted" ) +
+  geom_hline(yintercept = -log10(0.25),linetype="dotted" ) +
   theme(axis.ticks.y = element_blank(),
         axis.title.y = element_blank(),
         axis.text.y = element_text(hjust = 1),
@@ -75,17 +69,17 @@ g2 <- ggplot(dorothea_filtering, aes(x=gset, y=-log10(pvalue),
         axis.title.x = element_text(size = 9),
         legend.position = "none"
         #plot.margin = unit(c(0, 0, 0, 0), 'cm')
-        ) +
+  ) +
   scale_fill_manual(values = c("steelblue","indianred")) +
-  coord_flip() + ylab("-log10(p-value)")
+  coord_flip() + ylab("-log10(BH p-value)")
 
 # miRNAs
 
 miRNA_filtering = miRNA  %>% dplyr::slice(1:50) %>%
   #dplyr::filter(pvalue < 0.05) %>%
-  arrange(-log10(pvalue) * sign(NES)) %>%
+  arrange(-log10(adj_pvalue) * sign(NES)) %>%
   mutate(dir_reg = sign(NES),
-         log_pval = -log10(pvalue),
+         log_pval = -log10(adj_pvalue),
          dir_col = dir_reg * log_pval,
          gset = RegulonName)
 
@@ -97,17 +91,13 @@ miRNA_filtering$gset = unlist(lapply(strsplit(miRNA_filtering$gset,
                                               "_"), 
                                      function(x) x[1]))
 
-miRNA_filtering = mutate(miRNA_filtering, gset = ifelse(adj_pvalue<=0.15,
-                     paste("*",gset,sep=""),
-                     gset))
-
 miRNA_filtering$gset = factor(miRNA_filtering$gset,
                               levels = miRNA_filtering$gset)
 
-g3 <- ggplot(miRNA_filtering, aes(x=gset, y=-log10(pvalue),
-                                     fill = factor(sign(NES)))) + 
+g3 <- ggplot(miRNA_filtering, aes(x=gset, y=log_pval,
+                                  fill = factor(sign(NES)))) + 
   geom_bar(stat = "identity") + theme_minimal() +
-  geom_hline(yintercept = -log10(0.05),linetype="dotted" ) +
+  geom_hline(yintercept = -log10(0.25),linetype="dotted" ) +
   theme(axis.ticks.y = element_blank(),
         axis.title.y = element_blank(),
         axis.title.x = element_text(size = 9),
@@ -117,23 +107,19 @@ g3 <- ggplot(miRNA_filtering, aes(x=gset, y=-log10(pvalue),
         #plot.margin = unit(c(0, 0, 0, 0), 'cm')
   ) +
   scale_fill_manual(values = c("steelblue","indianred")) +
-  coord_flip() + ylab("-log10(p-value)")
+  coord_flip() + ylab("-log10(BH p-value)")
 
 upper_panel = plot_grid(g1,g2,g3,align = "h", rel_widths = c(1,.45,.45),
-          nrow = 1)
+                        nrow = 1)
 
 # PROGENy filtering
 
 progeny_filtering = progeny %>%
-  arrange(-log10(progeny_pvals + 0.00000001) * sign(progeny_scores)) %>%
+  arrange(-log10(adj_pvalues + 0.00000001) * sign(progeny_scores)) %>%
   mutate(dir_reg = sign(progeny_scores),
-         log_pval = -log10(progeny_pvals + 0.00000001),
-         gset = pathway,
-         adj_pval = p.adjust(progeny_pvals,method = "BH")) %>%
-  mutate(gset = ifelse(adj_pval<=0.15,
-                       paste("*",gset,sep=""),
-                       gset)) %>%
-  arrange((progeny_scores))
+         log_pval = -log10(adj_pvalues + 0.00000001) + 0.05,
+         gset = pathway) %>%
+  arrange(log_pval * dir_reg)
 
 progeny_filtering$gset = factor(progeny_filtering$gset,
                                 levels = progeny_filtering$gset)
@@ -152,8 +138,8 @@ g4 = ggplot(progeny_filtering,
         plot.margin = unit(c(0, 0, 8, 0), 'cm')
   ) +
   scale_fill_manual(values = c("steelblue","indianred")) + 
-  ylab("-log10(p-value)") + 
-  geom_hline(yintercept = -log10(0.05),linetype="dotted" ) +
+  ylab("-log10(BH p-value)") + 
+  geom_hline(yintercept = -log10(0.25),linetype="dotted" ) +
   coord_flip() + labs(fill="Direction")
 
 
